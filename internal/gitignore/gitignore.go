8c9efa6ab2ee359c672e497fd4b250f21370baa4
Kirill Belousov <cyrmax@internet.ru>
2025-10-17 16:44:45 +0300

    TestGitIgnore_Match passes match for simple file

--- internal/gitignore/gitignore.go
+++ internal/gitignore/gitignore.go
@@ -1,24 +1,36 @@
 package gitignore
 
-// GitIgnore holds an ordered collection of parsed patterns and a base path.
-type GitIgnore struct {
-	// TODO: to be implemented
+// ignorePattern holds a single parsed pattern.
+type ignorePattern struct {
+	Pattern string
+	Negates bool
 }
 
-// New creates a new GitIgnore with no patterns.
+// GitIgnore holds an ordered collection of parsed patterns and a base path.
+type GitIgnore struct {
+	basePath string
+	patterns []ignorePattern
+}
+
+// New creates a new GitIgnore with no patterns. The base path is not checked to be valid path.
 func NewGitIgnore(basePath string) *GitIgnore {
-	// TODO: implementation
-	return &GitIgnore{}
+	return &GitIgnore{basePath: basePath}
 }
 
 // Match returns true if a given path matches against a collection of added patterns and false otherwise
 func (gi *GitIgnore) Match(path string) bool {
-	// TODO: implementation
-	return false
+	// FIXME: Not fully implemented yet
+	shouldIgnore := false
+	for _, pat := range gi.patterns {
+		if pat.Pattern == path {
+			shouldIgnore = true
+		}
+	}
+	return shouldIgnore
 }
 
 // AppendPattern adds a new pattern to a collection of patterns
 func (gi *GitIgnore) AppendPattern(pattern string) {
-	// TODO: implementation
-
+	// FIXME: Not fully implemented yet
+	gi.patterns = append(gi.patterns, ignorePattern{Pattern: pattern, Negates: false})
 }
